54dfdaafdd8fad8af6ddf9ad7e31b7a5bf83dfa9
tobi_martins <62524527+tomadar@users.noreply.github.com>
2025-08-24 19:10:16 +0100

    Update nsdset.py

--- nsdset.py
+++ nsdset.py
@@ -1,10 +1,35 @@
 # Number, Seperation and Differences by
-# Oluwatobi Martins; Created Martinga formular
-# 18th - 21st August, 2025
+# Tobi Martins; Created Martinga formular
+# 21st August, 2025
 
 import os
 import time
 
 def nsdset():
 	try:
-		
+		os.system('clear')
+		print("""
+			Number, Seperation and Differences
+			by Tobi Martins; Martinga Formular
+""")
+		n = int(input("Number: "))
+		d = int(input("Difference: "))
+		s = int(input("Seperation: "))
+		try:
+			f = n/s - ((d/2)+((d*s)/2)-d)
+			print(f)
+			for i in range(s-1):
+				f = f+d
+				print(f)
+		except ZeroDivisionError:
+			print("Sorry Zero is Indenominable!")
+			time.sleep(2)
+			os.system('clear')
+			nsdset()
+	except ValueError:
+		print("Only numbers Supported!")
+		time.sleep(2)
+		os.system("clear")
+		nsdset()
+
+nsdset()
